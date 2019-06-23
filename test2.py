897f9fd17a384fb6a458c09fb4aa60a157751758
Alexey <4pok45@mail.ru>
2019-06-24 01:08:50 +0300

    added new tests

--- test2.py
+++ test2.py
@@ -1,36 +1,42 @@
+import pytest
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
+import time
 import math
 
 
-def calc(x):
-    return str(math.log(abs(12*math.sin(x))))
+@pytest.fixture(scope="function")
+def calc():
+    return str(math.log(int(time.time())))
 
 
-opt = webdriver.ChromeOptions()
-opt.add_experimental_option('w3c', False)
-browser = webdriver.Chrome(chrome_options=opt)
-browser.get("http://suninjuly.github.io/explicit_wait2.html")
-
-# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
-price = WebDriverWait(browser, 12).until(
-        EC.text_to_be_present_in_element((By.ID, "price"), '10000 RUR')
-    )
-
-button1 = browser.find_element_by_id('book')
-button1.click()
-
-x = browser.find_element_by_id('input_value')
-x = int(x.text)
-
-y = calc(x)
-
-input1 = browser.find_element_by_id('answer')
-input1.send_keys(y)
-
-button2 = browser.find_element_by_id('solve')
-button2.click()
+@pytest.fixture(scope="function")
+def driver():
+    print("\nstart browser for test..")
+    opt = webdriver.ChromeOptions()
+    opt.add_experimental_option('w3c', False)
+    driver = webdriver.Chrome(chrome_options=opt)
+    yield driver
+    print("\nquit browser..")
+    driver.quit()
 
 
+@pytest.mark.parametrize('link', ['https://stepik.org/lesson/236895/step/1', 'https://stepik.org/lesson/236896/step/1',
+                                  'https://stepik.org/lesson/236897/step/1', 'https://stepik.org/lesson/236898/step/1',
+                                  'https://stepik.org/lesson/236899/step/1', 'https://stepik.org/lesson/236903/step/1',
+                                  'https://stepik.org/lesson/236904/step/1', 'https://stepik.org/lesson/236905/step/1'])
+class TestPagesOfStepik():
+    def test_guest_should_see_login_link(self, driver, link, calc):
+        driver.get(link)
+        # Ищем строку и вводим ответ из фикстуры calc
+        input_answer = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'textarea')))
+        input_answer.send_keys(calc)
+        # Ждем, пока появится кнопка отправки и нажимаем на нее
+        button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'submit-submission ')))
+        button.click()
+        # Проверяем, что ответ правильный
+        answer = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'smart-hints__hint')))
+        value = answer.text
+        assert value == 'Correct!', 'Answer not correct'
